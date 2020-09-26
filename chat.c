9d8e2ffefe71ef1cef71fc4fdb3fee1fd4cec853
Cole Smith <colehsmith38@gmail.com>
2020-09-26 14:55:01 -0400

    Add arg handling

--- chat.c
+++ chat.c
@@ -1,17 +1,86 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
+int isvalidport(char *port);
+int isvalidip(char *ip_addr);
+
+/* Store IP address string in sockaddr_in struct:
+ * inet_pton(AF_INET "192.168.0.1", &(sa.sin_addr));
+ */
+
 int main(int argc, char **argv) {
-    int aflag = 0;
-    int bflag = 0;
-    char *cvalue = NULL;
+    char *ip_addr = NULL;
+    char *port = NULL;
     int index;
     int c;
 
     opterr = 0;
-
-    while ((c = getopt(argc, argv, "")))
+    
+    if (argc == 0) {
+        // set to server
+    }
+    else {
+        // handle arguments
+    }
+    while ((c = getopt(argc, argv, "hp:s:")) != -1)
+        switch(c) {
+            case 'h':
+                // print help message
+                printf("Help Message\n");
+                return 0;
+            case 's':
+                ip_addr = optarg;
+                printf("ip address: %s\n", ip_addr);
+                if (isvalidip(ip_addr))
+                    printf("IP is valid\n");
+                else
+                    printf("IP is not valid\n");
+                // verify valid input
+                break;
+            case 'p':
+                port = optarg;
+                printf("port: %s\n", port);
+                if (isvalidport(port))
+                    printf("Port is valid\n");
+                else
+                    printf("Port is not valid\n");
+                // verify valid input
+                //
+                break;
+            case '?':
+                if (optopt == 's' || optopt == 'p')
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr, "Unknown option '-%c'.\n", optopt);
+                else
+                    fprintf(stderr, "Unknown option character '\\x%x'.\n", optopt);
+                return 1;
+            default:
+                printf("No arguments provided\n");
+                abort();
+                
+        }
 	return 0;
+}
+
+int isvalidport(char *port) {
+    int s = strlen(port);
+    for (int i = 0; i < s; i++) {
+        if (!isdigit(*port))
+            return 0;
+        port++;
+    }
+    return 1;
+}
+
+int isvalidip(char *ip_addr) {
+    int s = strlen(ip_addr);
+    for (int i = 0; i < s; i++) {
+        if (!isdigit(ip_addr[i]) && ip_addr[i] != '.') 
+            return 0;
+    } 
+    return 1;
 }
