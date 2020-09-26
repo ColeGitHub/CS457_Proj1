b5c369a3f2192f94129856eeb882f00e0f499705
Cole Smith <colehsmith38@gmail.com>
2020-09-26 17:56:42 -0400

    No clue

--- chat.c
+++ chat.c
@@ -1,8 +1,11 @@
 #include <ctype.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 int isvalidport(char *port);
 int isvalidip(char *ip_addr);
